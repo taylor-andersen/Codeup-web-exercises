48bf200def53ff5c9f5cab4e493ff1fe674a6b70
Taylor Andersen <tay.andersen99@gmail.com>
2023-10-12 16:01:51 -0500

    Saving work, began working on HTML. alert, confirm, prompt

---
+++ js/javascript-with-html.js
@@ -0,0 +1,17 @@
+"use strict"
+
+let myAge = 24
+console.log(myAge);
+
+//alert
+alert("Warning: I'm about to hit you with 1000 years of pain");
+
+//confirm
+let confirmed = confirm("Are you sure you're ready?");
+console.log(confirmed);
+
+//prompt
+let userInput = prompt("Please type in your name");
+console.log(`User is named ${userInput}`);
+
+console.log("This is my external js file")
