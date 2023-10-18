8ce3787dfc6f6d4bbed2059a288b91023da72686
Taylor Andersen <tay.andersen99@gmail.com>
2023-10-18 16:34:46 -0500

    Troubleshot and corrected mistakes inside of while.js.

--- js/while.js
+++ js/while.js
@@ -9,16 +9,14 @@ let amountOfCones = Math.floor(Math.random() * 50) + 50;
 
 do {
     let perPerson = Math.floor(Math.random() * 5) + 1;
-   if(amountOfCones > perPerson) {
-       console.log(`${perPerson} cones sold... only ${amountOfCones} left!`);
-       amountOfCones = amountOfCones - perPerson;
+   if(amountOfCones >= perPerson) {
+       amountOfCones -= perPerson;
+       console.log(`${perPerson} cones sold...${amountOfCones} left!`);
    } else if (amountOfCones < perPerson) {
        console.log(`Cannot sell you ${perPerson} cones, I only have ${amountOfCones} left.`)
-       amountOfCones = amountOfCones - perPerson;
-   } else if (amountOfCones === 0){
-       console.log('Yay! I sold them all!')
    }
-} while (amountOfCones > 0)
+} while (amountOfCones > 0);
+console.log("Yay! I sold them all!");
 
 // ------------------------------------------------
 
