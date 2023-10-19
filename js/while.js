c5e6d918955f5f2abf73096c51ed0cfd4c3a02d6
Taylor Andersen <tay.andersen99@gmail.com>
2023-10-19 09:41:36 -0500

    Completed daily warmup "while loop" Oct.19

--- js/while.js
+++ js/while.js
@@ -1,7 +1,7 @@
 let x = 2
 while(x < 65537) {
     console.log(x);
-    x = x * 2;
+    x *= 2;
 }
 //------------------------------------------------
 
@@ -9,13 +9,15 @@ let amountOfCones = Math.floor(Math.random() * 50) + 50;
 
 do {
     let perPerson = Math.floor(Math.random() * 5) + 1;
+
    if(amountOfCones >= perPerson) {
        amountOfCones -= perPerson;
        console.log(`${perPerson} cones sold...${amountOfCones} left!`);
-   } else if (amountOfCones < perPerson) {
+   } else {
        console.log(`Cannot sell you ${perPerson} cones, I only have ${amountOfCones} left.`)
    }
 } while (amountOfCones > 0);
+
 console.log("Yay! I sold them all!");
 
 // ------------------------------------------------
--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -1,37 +1,49 @@
-//Taylor Andersen
-//Daily Warm-Ups
-let name = "Taylor Andersen";
-let year = "2017";
-console.log(`My name is ${name} and I graduated high school in ${year}.`)
+// //Taylor Andersen
+// //Daily Warm-Ups
+// let name = "Taylor Andersen";
+// let year = "2017";
+// console.log(`My name is ${name} and I graduated high school in ${year}.`)
+//
+// //Create a function that takes in a string, and returns the string with the first letter in uppercase.
+// function capitalizeFirstLetter(string) {
+//     //Grab first letter, character 0, uppercase it, attach onto beginning of the original string.
+//     console.log(string);
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//
+// }
+// let capitalizedString = capitalizeFirstLetter("example")
+// console.log(capitalizedString);
+// let capitalizedSentence = capitalizeFirstLetter("i'm a sentence.");
+// console.log(capitalizedSentence);
+//
+// //create a function named 'largestNum' that takes in three numbers as input and returns the largest of the three
+//
+// function largestNum(num1, num2, num3) {
+//     if(num1 > num2 && num1 > num3) {
+//         return num1;
+//     }
+//     else if(num2 > num1 && num2 > num3) {
+//         return num2;
+//     }
+//     else if(num3 > num1 && num3 > num2) {
+//         return num3;
+//     }
+// }
+//
+// console.log(largestNum(5,10,15));
 
-//Create a function that takes in a string, and returns the string with the first letter in uppercase.
-function capitalizeFirstLetter(string) {
-    //Grab first letter, character 0, uppercase it, attach onto beginning of the original string.
-    console.log(string);
-    return string.charAt(0).toUpperCase() + string.slice(1);
+//-------------------------------------------------------------
 
+let num = 100;
+console.log(num + ' is even');
+while(num > 0 ) {
+    num -= 5;
+    if (num % 2 === 0) {
+        console.log(num + ' is even');
+    } else {
+        console.log(num)
+    }
 }
-let capitalizedString = capitalizeFirstLetter("example")
-console.log(capitalizedString);
-let capitalizedSentence = capitalizeFirstLetter("i'm a sentence.");
-console.log(capitalizedSentence);
 
-//create a function named 'largestNum' that takes in three numbers as input and returns the largest of the three
-
-function largestNum(num1, num2, num3) {
-    if(num1 > num2 && num1 > num3) {
-        return num1;
-    }
-    else if(num2 > num1 && num2 > num3) {
-        return num2;
-    }
-    else if(num3 > num1 && num3 > num2) {
-        return num3;
-    }
-}
-
-console.log(largestNum(5,10,15));
-
-
-
+//--------------------------------------------------------------
 
