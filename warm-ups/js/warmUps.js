1e2ef8d6d44ca78dfd7e000d518d61e56d6a55eb
Taylor Andersen <tay.andersen99@gmail.com>
2023-11-06 09:35:29 -0600

    Completed daily warmup

--- warm-ups/js/warmUps.js
+++ warm-ups/js/warmUps.js
@@ -185,3 +185,17 @@ function changeColor (arrOfElements, bgColor, textColor) {
 }
 
 console.log(changeColor(elements,'red', 'purple'))
+
+//------------------------------------------------------------------
+
+for (let i = 1; i <= 100; i++) {
+    if (i %3 ===0 && i %5 ===0) {
+        console.log("FizzBuzz")
+    } else if (i %5 === 0) {
+        console.log("Buzz")
+    } else if (i %3 ===0) {
+        console.log("Fizz")
+    } else {
+        console.log(i);
+    }
+}
