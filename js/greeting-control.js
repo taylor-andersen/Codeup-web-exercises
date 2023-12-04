9d543a666d675f5c770b4b1dab5d44b2ad253f86
Taylor Andersen <tay.andersen99@gmail.com>
2023-12-04 15:28:07 -0600

    Completed greeter exercise regarding modules

--- js/greeting-control.js
+++ js/greeting-control.js
@@ -2,8 +2,9 @@ import randomGreeting from "./greeting-logic.js";
 
 let name = document.getElementById('name');
 
-document.querySelector('button').addEventListener("click", () => {
-    document.getElementById('greetingMessage').innerText = randomGreeting() + name + '!'
+document.querySelector('button').addEventListener("click", (event) => {
+    event.preventDefault();
+    document.getElementById('greetingMessage').innerText = randomGreeting() + ' ' + name.value + '!'
 })
 
 
